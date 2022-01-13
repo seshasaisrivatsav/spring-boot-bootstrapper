e568b4d6a072ecc470bc538a11b6370428c62684
seshasaisrivatsav <seshasaisrivatsav@gmail.com>
2022-01-13 13:20:25 -0500

    added env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+export M2_HOME=/Users/skuchibhotla/learn/apache-maven-3.8.1
+export M2=$M2_HOME/bin
+export MAVEN_OPTS=-Xms1024m
+export PATH=$M2:$PATH
+git config user.name "seshasaisrivatsav"
+git config user.email "seshasaisrivatsav@gmail.com"
